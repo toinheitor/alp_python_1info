bd10719eedb8065821d9ee9317418a6d7bea206b
toinheitor <heitorchaves0113@gmail.com>
2026-06-26 09:06:35 -0300

    Add presenter surname greeting logic

---
+++ questao5.py
@@ -0,0 +1,10 @@
+sobrenome = input("Digite o sobrenome do apresentador: ").upper()
+
+if sobrenome == "PINHEIRO" or sobrenome == "ARAÚJO":
+    print("Bom Dia Nação")
+
+elif sobrenome == "BONNER" or sobrenome == "VASCONCELOS":
+    print("Jornal Brasileiro")
+
+else:
+    print("Apresentador(a) desconhecido(a).")
